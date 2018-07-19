112a3a826d25a98e0664180a813927c13c7b1c4b
dongkyu_kim <donggui7@gmail.com>
2018-07-19 19:49:35 +0900

    add emacs packages - emmet solarized

--- init.el
+++ init.el
@@ -25,15 +25,42 @@
 ;; 終了時にオートセーブファイルを削除する
 (setq auto-save-default nil)
 
-; ;; load-path で ~/.emacs.d とか書かなくてよくなる
-; (when load-file-name
-;   (setq user-emacs-directory (file-name-directory load-file-name)))
-;
-; ;; el-get
-; (add-to-list 'load-path (locate-user-emacs-file "el-get"))
-; (require 'el-get)
-; ;; el-getでダウンロードしたパッケージは ~/.emacs.d/ に入るようにする
-; (setq el-get-dir (locate-user-emacs-file ""))
-;
-; ;; auto-complage
-; (el-get-bundle auto-complete)
+;;; *.~ とかのバックアップファイルを作らない
+(setq make-backup-files nil)
+;;; .#* とかのバックアップファイルを作らない
+(setq auto-save-default nil)
+
+;; vcを起動しないようにする
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (color-theme-solarized emmet-mode)))
+ '(vc-handled-backends nil))
+
+;; package 管理
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; theme
+(require 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; マークアップ言語全部で使う
+(add-hook 'css-mode-hook  'emmet-mode) ;; CSSにも使う
+(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent はスペース2個
+(eval-after-load "emmet-mode"
+ '(define-key emmet-mode-keymap (kbd "C-j") nil)) ;; C-j は newline のままにしておく
+  (keyboard-translate ?\C-i ?\H-i) ;;C-i と Tabの被りを回避
+  (define-key emmet-mode-keymap (kbd "H-i") 'emmet-expand-line) ;; C-i で展開
+;; theme
+(set-terminal-parameter nil 'background-mode 'dark)
+(load-theme 'solarized t) 
